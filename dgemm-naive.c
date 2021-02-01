6e5bca5c1dae0ef76ce778ed41bcf49c9224d671
ArnaudFickinger <arnaud.fickinger@berkeley.edu>
2021-01-31 20:14:15 -0800

    add simd instructions

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -1,4 +1,9 @@
-const char* dgemm_desc = "Naive, three-loop dgemm.";
+// SSE SIMD intrinsics
+#include <xmmintrin.h>
+// AVX SIMD intrinsics
+#include <immintrin.h>
+
+const char* dgemm_desc = "SIMD dgemm.";
 
 /*
  * This routine performs a dgemm operation
@@ -8,14 +13,20 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  */
 void square_dgemm(int n, double* A, double* B, double* C) {
     // For each row i of A
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i+=4) {
         // For each column j of B
-        for (int j = 0; j < n; ++j) {
-            // Compute C(i,j)
-            double cij = C[i + j * n];
+        for (int j = 0; j < n; j++) {
+            _m256d c0 = {0,0,0,0};
+//            double cij = C[i + j * n];
             for (int k = 0; k < n; k++) {
-                cij += A[i + k * n] * B[k + j * n];
+                c0 = _m256_add_pd(
+                        c0,
+                        _mm256_mul_pd(
+                                _mm256_load_pd(a+i+k*n),
+                                _mm256_broadcast_sd(b+k+k*N)));
+//                cij += A[i + k * n] * B[k + j * n];
             }
+            _mm256_store_pd(c+i+j*n, c0);
             C[i + j * n] = cij;
         }
     }
