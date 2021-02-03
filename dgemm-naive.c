aace8948ae26a00f282f14dcc2ef29ae8d423653
ArnaudFickinger <arnaud.fickinger@berkeley.edu>
2021-02-02 22:49:48 -0800

    add simd instructions

--- dgemm-naive.c
+++ dgemm-naive.c
@@ -57,21 +57,21 @@ const char *dgemm_desc = "SIMD dgemm.";
 //    }
 //}
 
-void square_dgemm(int n, double *A, double *B, double *C) {
-    for (int k = 0; k < n; k++) {
-        for (int j = 0; j < n; j++) {
-            __m512d bkj = _mm512_set1_pd(B[k + j * n]);
-            int i = 0;
-            for (; i < n - 8; i += 8) {
-                _mm512_storeu_pd(&C[i + j * n], _mm512_fmadd_pd(_mm512_loadu_pd(&A[i + k * n]), bkj,
-                                                                _mm512_loadu_pd(&C[i + j * n])));
-            }            //remaining elements
-//            for (; i < n; i++) {
-//                C[i + j * n] += A[i + k * n] * B[k + j * n];
-//            }
-        }
-    }
-}
+//void square_dgemm(int n, double *A, double *B, double *C) {
+//    for (int k = 0; k < n; k++) {
+//        for (int j = 0; j < n; j++) {
+//            __m512d bkj = _mm512_set1_pd(B[k + j * n]);
+//            int i = 0;
+//            for (; i < n - 8; i += 8) {
+//                _mm512_storeu_pd(&C[i + j * n], _mm512_fmadd_pd(_mm512_loadu_pd(&A[i + k * n]), bkj,
+//                                                                _mm512_loadu_pd(&C[i + j * n])));
+//            }            //remaining elements
+////            for (; i < n; i++) {
+////                C[i + j * n] += A[i + k * n] * B[k + j * n];
+////            }
+//        }
+//    }
+//}
 
 //void square_dgemm(int n, double *A, double *B, double *C) {
 //    for (int k = 0; k < n; k++) {
@@ -108,42 +108,48 @@ void square_dgemm(int n, double *A, double *B, double *C) {
 //        }
 //    }
 
-//void square_dgemm(int n, double *left, double *right, double *result) {
-//    const int block_width = n >= 256 ? 512 : 256;
-//    const int block_height = n >= 512 ? 8 : n >= 256 ? 16 : 32;
-//    for (int column_offset = 0; column_offset < n; column_offset += block_width) {
-//        for (int row_offset = 0; row_offset < n; row_offset += block_height) {
-//            for (int i = 0; i < n; ++i) {
-//                for (int j = column_offset; j < column_offset + block_width && j < n; j += 64) {
-//                    __m512d sum1 = _mm512_loadu_pd(result + i * n + j);
-//                    __m512d sum2 = _mm512_loadu_pd(result + i * n + j + 8);
-//                    __m512d sum3 = _mm512_loadu_pd(result + i * n + j + 16);
-//                    __m512d sum4 = _mm512_loadu_pd(result + i * n + j + 24);
-//                    __m512d sum5 = _mm512_loadu_pd(result + i * n + j + 32);
-//                    __m512d sum6 = _mm512_loadu_pd(result + i * n + j + 40);
-//                    __m512d sum7 = _mm512_loadu_pd(result + i * n + j + 48);
-//                    __m512d sum8 = _mm512_loadu_pd(result + i * n + j + 56);
-//                    for (int k = row_offset; k < row_offset + block_height && k < n; ++k) {
-//                        __m512d multiplier = _mm512_set1_pd(left[i * n + k]);
-//                        sum1 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j), sum1);
-//                        sum2 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 8), sum2);
-//                        sum3 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 16), sum3);
-//                        sum4 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 24), sum4);
-//                        sum5 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 32), sum5);
-//                        sum6 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 40), sum6);
-//                        sum7 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 48), sum7);
-//                        sum8 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 56), sum8);
-//                    }
-//                    _mm512_storeu_pd(result + i * n + j, sum1);
-//                    _mm512_storeu_pd(result + i * n + j + 8, sum2);
-//                    _mm512_storeu_pd(result + i * n + j + 16, sum3);
-//                    _mm512_storeu_pd(result + i * n + j + 24, sum4);
-//                    _mm512_storeu_pd(result + i * n + j + 32, sum5);
-//                    _mm512_storeu_pd(result + i * n + j + 40, sum6);
-//                    _mm512_storeu_pd(result + i * n + j + 48, sum7);
-//                    _mm512_storeu_pd(result + i * n + j + 56, sum8);
-//                }
-//            }
-//        }
-//    }
-//}
+void square_dgemm(int n, double *left, double *right, double *result) {
+    const int block_width = n >= 256 ? 512 : 256;
+    const int block_height = n >= 512 ? 8 : n >= 256 ? 16 : 32;
+    for (int column_offset = 0; column_offset < n; column_offset += block_width) {
+        for (int row_offset = 0; row_offset < n; row_offset += block_height) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = column_offset; j < column_offset + block_width && j < n; j += 64) {
+                    __m512d sum1 = _mm512_loadu_pd(result + i * n + j);
+                    __m512d sum2 = _mm512_loadu_pd(result + i * n + j + 8);
+                    __m512d sum3 = _mm512_loadu_pd(result + i * n + j + 16);
+                    __m512d sum4 = _mm512_loadu_pd(result + i * n + j + 24);
+                    __m512d sum5 = _mm512_loadu_pd(result + i * n + j + 32);
+                    __m512d sum6 = _mm512_loadu_pd(result + i * n + j + 40);
+                    __m512d sum7 = _mm512_loadu_pd(result + i * n + j + 48);
+                    __m512d sum8 = _mm512_loadu_pd(result + i * n + j + 56);
+                    for (int k = row_offset; k < row_offset + block_height && k < n; ++k) {
+                        __m512d multiplier = _mm512_set1_pd(left[i * n + k]);
+                        sum1 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j), sum1);
+                        sum2 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 8), sum2);
+                        sum3 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 16), sum3);
+                        sum4 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 24), sum4);
+                        sum5 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 32), sum5);
+                        sum6 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 40), sum6);
+                        sum7 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 48), sum7);
+                        sum8 = _mm512_fmadd_pd(multiplier, _mm512_loadu_pd(right + k * n + j + 56), sum8);
+                    }
+                    _mm512_storeu_pd(result + i * n + j, sum1);
+                    _mm512_storeu_pd(result + i * n + j + 8, sum2);
+                    _mm512_storeu_pd(result + i * n + j + 16, sum3);
+                    _mm512_storeu_pd(result + i * n + j + 24, sum4);
+                    _mm512_storeu_pd(result + i * n + j + 32, sum5);
+                    _mm512_storeu_pd(result + i * n + j + 40, sum6);
+                    _mm512_storeu_pd(result + i * n + j + 48, sum7);
+                    _mm512_storeu_pd(result + i * n + j + 56, sum8);
+                }
+                for (; j < n; j++) {
+                    for (int k = row_offset; k < row_offset + block_height && k < n; ++k) {
+                        result[j + i * n] += left[k + i * n] * right[j + k * n];
+                    }
+                }
+            }
+        }
+    }
+
+}
